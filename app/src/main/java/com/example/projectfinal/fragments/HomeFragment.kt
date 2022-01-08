43afbe115dc0095515c9f9373158abfc6a5c63ee
Nicolas Al Khoury <nicolas.n.khoury@gmail.com>
2022-01-08 11:37:38 +0200

    small adjustments

--- app/src/main/java/com/example/projectfinal/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/projectfinal/fragments/HomeFragment.kt
@@ -1,13 +1,14 @@
-package com.example.projectfinal
+package com.example.projectfinal.fragments
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import com.example.projectfinal.Camera
+import com.example.projectfinal.R
+import com.example.projectfinal.TicTacToe
 
 class HomeFragment : Fragment() {
 
